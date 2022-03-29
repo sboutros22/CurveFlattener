b3ce583c4f4c036875dbf7d963e759087b69bbc4
Fadi Rached <74790979+FadiRached@users.noreply.github.com>
2022-03-29 14:50:57 -0400

    Add XlsxHandler

--- curveflattern/src/curveflattern/XlsxHandler.java
+++ curveflattern/src/curveflattern/XlsxHandler.java
@@ -2,8 +2,38 @@ package curveflattern;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.xmlbeans.impl.xb.ltgfmt.FileDesc.Role;
+import org.junit.jupiter.api.Test;
 
 public class XlsxHandler {
+    String fileLocation;
     FileInputStream file = new FileInputStream(new File(fileLocation));
-    Workbook workbook = new XSSFWorkbook(file);
+    XSSFWorkbook workbook = new XSSFWorkbook(file);
+    public void readXlsx()
+    {
+        XSSFSheet sheet = workbook.getSheetAt(0);
+        Map<Integer, List<String>> data = new HashMap<>();
+        int i = 0;
+        for (Role row : sheet) {
+        data.put(i, new ArrayList<String>());
+        for (Cell cell : row) {
+            data.put(i, new ArrayList<String>());
+            }
+        }
+        i++;
+    }
+
+@Test
+	public void happyXlsxFound() throws FileNotFoundException {
+		ArrayList ReadXlsData =  readXlsx("C://Code/test.csv");
+        ArrayList expectedData = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        boolean equal = expectedData.equals(ReadCsvData);
+        assertTrue(equal);
+	}
 }
