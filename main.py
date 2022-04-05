701744326c724dda38302dbee3e43f417c0bee41
ncontos522 <ncontos24@jcu.edu>
2022-04-05 15:03:25 -0400

    pushed from nick

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import csv
 
 import matplotlib.pyplot as plt
 from matplotlib import style
@@ -7,22 +8,22 @@ import pandas as pd
 def excelToCsv():
     df = pd.read_excel("C:\\Users\\johnd\\Downloads\\points.xlsx")
     df.to_csv('points.csv', index=None, header=True)
-excelToCsv()
+
 
 #another way of making a csv. Could be used later to manipulate points for the math conversion
-def createCsv():
-    header = ['x_coordinates, y_coordinates']
-    data = [
-    [1,2],
-    [2,8],
-    [7,60],
-    [8,4]
-    ]
-    with open('points.csv', 'w', encoding= 'UTF8', newline = '') as f:
-        writer = csv.writer(f)
-        writer.writerow(header)
-        writer.writerows(data)
-createCsv()
+# def createCsv():
+#     header = ['x_coordinates, y_coordinates']
+#     data = [
+#     [1,2],
+#     [2,8],
+#     [7,6],
+#     [8,4]
+#     ]
+#     with open('points.csv', 'w', encoding= 'UTF8', newline = '') as f:
+#         writer = csv.writer(f)
+#         writer.writerow(header)
+#         writer.writerows(data)
+# createCsv()
 
 #Displaying the current csv
 def readCsv():
