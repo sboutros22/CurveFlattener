17327cfc653c1059eb37bb5555b1fb16ecf23442
ncontos522 <ncontos24@jcu.edu>
2022-04-12 14:23:04 -0400

    pushed from nick

--- main.py
+++ main.py
@@ -4,38 +4,52 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import pandas as pd
 
-#takes excel file and converts it to csv
+
+
+# takes excel file and converts it to csv
 def excelToCsv():
     df = pd.read_excel("C:\\Users\\johnd\\Downloads\\points.xlsx")
     df.to_csv('points.csv', index=None, header=True)
 
-#contos was here
-#another way of making a csv. Could be used later to manipulate points for the math conversion
-# def createCsv():
-#     header = ['x_coordinates, y_coordinates']
-#     data = [
-#     [1,2],
-#     [2,8],
-#     [7,6],
-#     [8,4]
-#     ]
-#     with open('points.csv', 'w', encoding= 'UTF8', newline = '') as f:
-#         writer = csv.writer(f)
-#         writer.writerow(header)
-#         writer.writerows(data)
-# createCsv()
 
-#Displaying the current csv
+# contos was here
+# another way of making a csv. Could be used later to manipulate points for the math conversion
+def createCsv():
+    header = ['x_coordinates, y_coordinates']
+    data = [
+        [1, 2],
+        [2, 8],
+        [7, 4],
+        [8, 7]
+    ]
+
+    with open('points.csv', 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+        writer.writerows(data)
+createCsv()
+
+
+
+
+
+
+# Displaying the current csv
 def readCsv():
     df = pd.read_csv('points.csv')
     print(df)
+
+
 readCsv()
 
-#Displays the csv file in graph format. Also saves the graph as a .jpg
+
+# Displays the csv file in graph format. Also saves the graph as a .jpg
 def displayCsv():
     style.use('ggplot')
     df = pd.read_csv('points.csv')
     df.plot()
     plt.savefig('testplot.png')
     plt.show()
+
+
 displayCsv()
