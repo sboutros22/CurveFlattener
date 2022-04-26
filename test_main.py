bebc16a4ac5f0b0d4dfad525a3511aa45d04a77a
johndelvaux <jdelvaux24@jcu.edu>
2022-04-26 11:16:15 -0400

    added tests

--- test_main.py
+++ test_main.py
@@ -20,13 +20,106 @@ class Test(TestCase):
         except:
             print("No file exists")
 
+class TestFlattenFunction(TestCase):
+    def test_flatten_function(self):
+        data = [
+        [1, 2],
+        [2, 20],
+        [7, 20],
+        [8, 4]
+        ]
+        from main import lowerSlopeAgain
+        self.assertTrue(lowerSlopeAgain(data, 4) == [[1, 9.125], [2, 13.625], [7, 13.625], [8, 9.625]])
+
+   #should fail because I changed the data point from 2 to 50
+    def test_fail_flatten_function(self):
+        data = [
+        [1, 50],
+        [2, 20],
+        [7, 20],
+        [8, 4]
+        ]
+        from main import lowerSlopeAgain
+        self.assertFalse(lowerSlopeAgain(data, 4) == [[1, 9.125], [2, 13.625], [7, 13.625], [8, 9.625]])
+
+    #should fail because I changed teh lowerSlopeAgain scale from 4 to 2
+    def test_fail_scale_flatten_function(self):
+        data = [
+            [1, 2],
+            [2, 20],
+            [7, 20],
+            [8, 4]
+        ]
+        from main import lowerSlopeAgain
+        self.assertTrue(lowerSlopeAgain(data, 2) == [[1, 9.125], [2, 13.625], [7, 13.625], [8, 9.625]])
+
+    #should return false because the program does not accept a 0 slope
+    def test_fail_scale_flatten_function(self):
+        data = [
+            [1, 2],
+            [2, 20],
+            [7, 20],
+            [8, 4]
+        ]
+        from main import lowerSlopeAgain
+        self.assertFalse(lowerSlopeAgain(data, 0) == [[1, 9.125], [2, 13.625], [7, 13.625], [8, 9.625]])
 
 class TestExcelToCsv(TestCase):
     def test_excel_to_csv(self):
         from main import excelToCsv
-        self.assertFalse(excelToCsv() ==  [
-            [0,1, 1000],
-            [1,2,3],
-            [2,3,2],
-            [3,4,1]
+        self.assertFalse(excelToCsv() == [
+            [0, 1, 1000],
+            [1, 2, 3],
+            [2, 3, 2],
+            [3, 4, 1]
         ])
+
+class TestGetAverage(TestCase):
+
+    #should return true because it is being passed in that data and the output is 11.5
+    def test_getAverage(self):
+        from main import getAverage
+        loweredData = [[1, 9.125], [2, 13.625], [7, 13.625], [8, 9.625]]
+        self.assertTrue(getAverage(loweredData) == 11.5)
+
+    #should return false because I changed the 1 to a 5
+    def test_getAverage(self):
+        from main import getAverage
+        loweredData = [[5, 9.125], [2, 13.625], [7, 13.625], [8, 9.625]]
+        self.assertFalse(getAverage(loweredData) == 11.5)
+
+class EndToEnd(TestCase):
+    def test_create_csv(self):
+        from main import excelToCsv
+        self.assertFalse(excelToCsv() == [
+            [0, 1, 4],
+            [1, 2, 3],
+            [2, 3, 2],
+            [3, 4, 1]
+        ])
+    def test_readCsv(self):
+        from main import readCsv
+        self.assertFalse(readCsv() == [
+            [0, 1, 4],
+            [1, 2, 3],
+            [2, 3, 2],
+            [3, 4, 1]
+        ])
+
+    def test_getAverage(self):
+        from main import getAverage
+        loweredData = [[1, 9.125], [2, 13.625], [7, 13.625], [8, 9.625]]
+        self.assertTrue(getAverage(loweredData) == 11.5)
+
+    def test_flatten(self):
+        data = [
+            [1, 2],
+            [2, 20],
+            [7, 20],
+            [8, 4]
+        ]
+        from main import lowerSlopeAgain
+        self.assertTrue(lowerSlopeAgain(data,4) == [[1, 9.125], [2, 13.625], [7, 13.625], [8, 9.625]])
+
+
+
